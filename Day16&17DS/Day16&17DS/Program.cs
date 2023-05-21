97f2cbd77221eadc40f89b893cdde95e130a8c45
Prince Gupta <princegupta.pg46@gmail.com>
2023-05-21 15:43:33 +0530

    Anagram

---
+++ Day16&17DS/Day16&17DS/Program.cs
@@ -0,0 +1,11 @@
+﻿using DelegatesAndEvents;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Data Structure");
+        Anagram anagram = new Anagram();
+        anagram.CheckAnagram();
+    }
+}
