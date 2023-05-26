9a95d38ba5b45c716fab9a858ce7a765408b4a5c
Prince Gupta <princegupta.pg46@gmail.com>
2023-05-26 22:30:20 +0530

    MergeSort

--- Day16&17DS/Day16&17DS/Program.cs
+++ Day16&17DS/Day16&17DS/Program.cs
@@ -1,11 +1,25 @@
-﻿using DelegatesAndEvents;
+﻿using DataStructure;
+using Day16_17DS;
+using DelegatesAndEvents;
 
 public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to Data Structure");
-        Anagram anagram = new Anagram();
-        anagram.CheckAnagram();
+        // Console.WriteLine("Welcome to Data Structure");
+        //Anagram anagram = new Anagram();
+        //anagram.CheckAnagram();
+
+        //BinarySearch search = new BinarySearch();
+        // InsertionSort sort1 = new InsertionSort();
+
+        //BubbleSort bubbleSort = new BubbleSort();
+        //bubbleSort.ReadFile3("F:\\BL_.net278\\Demo\\Day16-17BL\\Day16&17DS\\Day16&17DS\\ABC.txt");
+        string[] Arr= { "sun", "earth", "mars", "mercury" };  
+        string[] a = MergeSort.mergeSort(Arr, 0, Arr.Length - 1);
+        for (int i = 0; i < a.Length; i++)
+        {
+            Console.WriteLine(a[i]);
+        }
     }
 }
